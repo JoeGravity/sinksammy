3be14de12acb50a6be416631088f093686ef9367
Joe Pfahl <joegravity@Joe-Pfahls-Mac-Pro.local>
2018-04-16 08:21:48 -0500

    Initial files

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  port: 8889,
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "cat_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
